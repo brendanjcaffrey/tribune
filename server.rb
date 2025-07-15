3ff121d34e38a496d8556e72c7f9a32b98ca5fcd
Brendan Caffrey <brendan@jcaffrey.com>
2025-07-14 21:21:14 -0500

    list newsletters

--- server.rb
+++ server.rb
@@ -2,10 +2,13 @@
 
 require 'connection_pool'
 require 'sinatra'
+require 'sinatra/contrib'
 require 'json'
 require 'pg'
 
 NEWSLETTERS_PATH = "#{File.expand_path(__dir__)}/newsletters"
+GET_QUERY = 'SELECT id, title, author, filename, updated_at FROM newsletters;'
+GET_AFTER_QUERY = 'SELECT id, title, author, filename, updated_at FROM newsletters WHERE updated_at > $1;'
 CREATE_QUERY = 'INSERT INTO newsletters (title, author, filename) VALUES ($1, $2, $3);'
 
 db = PG.connect(
@@ -15,6 +18,28 @@ db = PG.connect(
   host: 'localhost',
   port: 5432
 )
+
+get '/newsletter' do
+  if params[:after]
+    begin
+      after_time = Time.parse(params[:after])
+    rescue ArgumentError => e
+      halt 400, "Invalid date format: #{e.message}"
+    end
+    result = db.exec_params(GET_AFTER_QUERY, [after_time])
+  else
+    result = db.exec(GET_QUERY)
+  end
+
+  vals = result.values.to_a.map do |row|
+    { id: row[0],
+      title: row[1],
+      author: row[2],
+      filename: row[3],
+      updated_at: row[4] }
+  end
+  json vals
+end
 
 post '/newsletter' do
   halt 400, 'Missing file' if !params[:file] || !(tempfile = params[:file][:tempfile])
