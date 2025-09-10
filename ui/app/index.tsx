29586aeb6fb1c1682d059603a1d3992ae2edb347
Brendan Caffrey <brendan@jcaffrey.com>
2025-09-10 08:11:13 -0500

    remove react native ui

--- ui/app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "@/hooks/useAuth";
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  const { isLoggedIn } = useAuth();
-
-  return <Redirect href={isLoggedIn ? "/newsletters" : "/sign-in"} />;
-}
